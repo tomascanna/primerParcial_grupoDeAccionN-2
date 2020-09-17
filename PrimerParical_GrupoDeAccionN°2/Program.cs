328465d72e1c4d77a2bf823a392acf30e5fba747
Tomas Cannatella <tomas742011@gmail.com>
2020-09-16 21:54:22 -0300

    primer commit

---
+++ PrimerParical_GrupoDeAccionN°2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParical_GrupoDeAccionN_2
+{
+    class Program
+    {
+        static void cargarCarrito(ref int cantidadCamisas)//Agregar camisas al carrito
+        {
+            cantidadCamisas++;
+            Console.Clear();
+            Console.WriteLine("Usted ha agregado una camisa al carrito");
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+        }
+
+        static void eliminarUnElementoCarrito(ref int cantidadCamisas)//Eliminar camisas
+        {
+            Console.Clear();
+            if (cantidadCamisas <= 0)//Si la variable de cantidad camisas esta en 0 no modificamos la variable y imprimimos un mensaje por pantalla
+            {
+                Console.WriteLine("No hay ningun elemento en el carrito para elminar");
+            }
+            else
+            {
+                cantidadCamisas--;
+                Console.WriteLine("Usted ha quitado una camisa del carrito");
+            }
+            
+            
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+        }
+
+        static double precioSinDescuento(int cantidadCamisa,double precioCamisa)//Calculamos el precio sin descuento de las camisas
+        {
+            double preciototal=0;
+
+            preciototal = (cantidadCamisa * precioCamisa);
+
+            return preciototal;
+        }
+
+        static double aplicarDescuento(int cantidadCamisa,double preciototal,ref double preciofinal)//Determinamos el descuento a aplicar
+        {
+
+            int tipoDescuento=0;
+
+            if (cantidadCamisa >= 3 && cantidadCamisa<=5)//Segun las cantidad de camisas es el descuento al cual le corresponde
+            {
+                tipoDescuento = 10;
+            }
+            else if (cantidadCamisa > 5)
+            {
+                tipoDescuento = 20;
+
+            }
+            else
+            {
+                tipoDescuento = 0;
+            }
+
+            preciofinal = (preciototal - ((preciototal * tipoDescuento) / 100));
+
+            return tipoDescuento;
+        }
+        static void Main(string[] args)
+        {
+
+            
+
+            string salida="";
+            double precioCamisa=1000;
+            int opc=0,cantidadCamisas=0;
+            double preciofinal = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Camisas PRADBIT - Ventas minoristas y mayoristas");
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Digite  1: Añadir camisa al carro de compra \n"); 
+                Console.WriteLine("Digite  2: Eliminar camisa del carro de compra \n");
+                Console.WriteLine("Digite  3: Salir \n");
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.WriteLine("     -   Cantidad de camisas en el carro de compras: {0} \n ", cantidadCamisas);
+                Console.WriteLine("     -   Precio unitario: $1000 \n");
+                Console.WriteLine("     -   Precio total sin descuento: ${0} \n", precioSinDescuento(cantidadCamisas,precioCamisa));
+                Console.WriteLine("     -   Tipo de descuento aplicado: %{0} \n", aplicarDescuento(cantidadCamisas, precioSinDescuento(cantidadCamisas, precioCamisa), ref preciofinal));
+                Console.WriteLine("     -   Precio final con descuento: ${0} \n", preciofinal);
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.Write("Elija una opcion: ");
+                try
+                {
+                    opc = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    opc = 0;//Opc se restaura en 0 para que no siga ejecutando la opcion seleccionada anteriormente en caso de ingresar un tipo de dato diferente al pedido.
+                }
+                
+
+                switch (opc)
+                {
+                    case 1://Agregar camisas al carrito
+                        {
+                            cargarCarrito(ref cantidadCamisas);
+                            break;
+                        }
+                    case 2://Elminamos camisas del carrito
+                        {
+                            eliminarUnElementoCarrito(ref cantidadCamisas);
+                            break;
+                        }
+                    case 3://Salida del programa
+                        {
+                            Console.Clear();
+                            do
+                            {
+                                Console.WriteLine("Estas seguro que desea salir? s/n");
+                                try
+                                {
+                                    salida = Console.ReadLine();
+                                }
+                                catch (Exception ex)
+                                {
+                                    string error = ex.Message;
+                                }
+                            } while (salida != "s" && salida != "n" && salida != "N" && salida != "S");//Validar dato ingresado
+
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("La opcion ingresada no es correcta.Presione una tecla para continuar");
+                            Console.ReadKey();
+                            break;
+                        }
+                }
+            } while (salida != "s" && salida != "S");//Condicion para salir
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+Grupo de Accion N°2 Cannatella-Schmidt
+
+1. ¿Qué diferencia hay entre un error de sintaxis y un error de lógica (falla)?
+	Los errores de sintaxis son los más fáciles de corregir, ya que nuestro error está justamente en un mal formulado de la sentencia, puede ser un error de tipeo o la falta de algún signo. 
+	En cambio un error de lógica es aquel en el cual nos equivocamos en el planteo del algoritmo, el cual se da por una mala comprensión o simplemente por que no se diseñó bien antes de llevarlo a lenguaje código. 
+
+2. ¿Cuáles son los operadores de una expresión lógica?
+	Los operadores lógicos son AND (&&), OR (||) y NOT (!).
+
+3. La refactorización de código: ¿Agrega/quita funcionalidad al programa?
+	No lo hace, ya que se realizan las correcciones que hagan falta en el código pero sin alterar el resultado o funcionalidad del mismo.
+
+4. Dentro de un ArrayList: ¿se pueden insertar valores de distinto tipo?
+	Si, ya que el ArrayList permite almacenar datos de distintos tipos y cada vez que agregamos un dato nuevo este se almacena en la ultima posicion.Para poder insertar un elemento en una posicion en concreto se hace con la funcion: <nombre_ArrayList>.Insert(<indice del ArrayList>,<dato>); y para agregar un elemento al ArrayList se hace con la funcion: <nombre_ArrayList>.Add(<dato>); 
+	
